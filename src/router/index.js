b389620b69e94122348b4b7cf643278eb02f810e
jonkse <jonkse@163.com>
2018-02-06 04:06:08 +0800

    6.1 商品列表组建拆分 12:31

--- src/router/index.js
+++ src/router/index.js
@@ -1,41 +1,53 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import GoodList from '@/views_old/GoodList.vue'
-import Title from '@/views_old/Title'
-import Image from '../views_old/Image.vue'
-import Cart from '@/views_old/Cart'
-import HelloWorld from './../components/HelloWorld'
+// import GoodList from '@/views_old/GoodList.vue'
+// import Title from '@/views_old/Title'
+// import Image from '../views_old/Image.vue'
+// import Cart from '@/views_old/Cart'
+import GoodsList from '@/views/GoodsList';
+
+
 Vue.use(Router);
 
 export default new Router({
-  // mode:"hash",
-  routes: [
-    {
-      path: '/',
-      name: 'GoodList',
-      //component: GoodList
-      // children:[
-      //   {
-      //     path:'title',
-      //     name:'title',
-      //     component:Title
-      //   },
-      //   {
-      //     path:'image',
-      //     name:'image',
-      //     component:Image
-      //   }
-      // ]
-      components:{
-        default:GoodList,
-        title:Title,
-        image:Image
-      }
-    },
-    {
-      path:'/cart/:cartId',
-      name:'cart',
-      component:Cart
-    }
-  ]
+  routes: [{
+    path: '/',
+    component: GoodsList
+  }]
+
 })
+
+
+//
+// export default new Router({
+//   // mode:"hash",
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'GoodList',
+//       //component: GoodList
+//       // children:[
+//       //   {
+//       //     path:'title',
+//       //     name:'title',
+//       //     component:Title
+//       //   },
+//       //   {
+//       //     path:'image',
+//       //     name:'image',
+//       //     component:Image
+//       //   }
+//       // ]
+//       components:{
+//         default:GoodList,
+//         title:Title,
+//         image:Image
+//       }
+//     },
+//     {
+//       path:'/cart/:cartId',
+//       name:'cart',
+//       component:Cart
+//     }
+//   ]
+// })
