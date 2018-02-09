d885ed36e86a3797de6f4c7129e7159c162dea80
jonkse <jonkse@163.com>
2018-02-09 16:26:06 +0800

    7.3 静态页面 10%

---
+++ server/Server.js
@@ -0,0 +1,17 @@
+let http = require('http');
+let url = require('url');
+let util = require('util');
+
+let server = http.createServer((req,res)=>{
+  res.statusCode = 200;
+  res.setHeader = ("Content-Type","text/plain;charset=utf-8");
+  
+  
+  urlAdr = url.parse(req.url);
+  utilObjStr = util.inspect(urlAdr);
+  res.end(utilObjStr);
+});
+
+server.listen(3000,'127.0.0.1', ()=>{
+  console.log("服务器已经运行，请打开浏览器，输入http://127.0.0.1：3000/来访问")
+})
